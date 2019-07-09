d90f503a0918c9403e5717dbcc4494f6ea08dd25
jenea007pon <40460815+jenea007pon@users.noreply.github.com>
2019-07-09 16:01:42 +0300

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -54,6 +54,18 @@ print(maxStr('I am',' z'))
 # вам необходимо получить на выходе словарь, где ключ - имя, значение - зарплата.
 # Запишите результаты в файл salary.txt так, чтобы на каждой строке было 2 столбца,
 # столбцы разделяются пробелом, тире, пробелом. в первом имя, во втором зарплата, например: Vasya - 5000
+people = ['Andry', 'Anya', 'David']
+salary = [100000, 75000, 150000]
+peopleSalary = dict(zip(people, salary))
+print(peopleSalary)
+
+f = open('D:\\ponamarev\\distr\\python\\danilov\\text.txt', 'w')  #ф-я открытия файла
+f.write('Name - Salary \n')
+for k, v in peopleSalary.items():
+    tempStr = str(k) + ' - ' + str(v) + '\n'
+    f.write(tempStr)
+f.close()
+
 # После чего прочитайте файл, выведите построчно имя и зарплату минус 13% (налоги ведь),
 # Есть условие, не отображать людей получающих более зарплату 500000, как именно
 #  выполнить условие решать вам, можете не писать в файл
@@ -61,19 +73,16 @@ print(maxStr('I am',' z'))
 #  если скажем эти файлы потом придется передавать.
 # Так же при выводе имя должно быть полностью в верхнем регистре!
 # Подумайте вспоминая урок, как это можно сделать максимально кратко, используя возможности языка Python.
+import csv
 
-people = ['Andry', 'Anya', 'David']
-salary = [100000, 75000, 150000]
-peopleSalary = dict(zip(people, salary))
-print(peopleSalary)
-
-f = open('D:\\ponamarev\\distr\\python\\danilov\\text.txt', 'w')  #ф-я открытия файла
-for k, v in peopleSalary.items():
-    f.write(str(k) + ' - ' + str(v) + '\n')
-
-f.close()
-
-#не успел доделать :(
+import csv
+ls = [];
+with open("D:\\ponamarev\\distr\\python\\danilov\\text.txt") as csvfile:
+    reader = csv.DictReader(csvfile, delimiter="-")
+    for row in reader:
+        print(row)
+        
+        #не успел доделать
 
 
 
